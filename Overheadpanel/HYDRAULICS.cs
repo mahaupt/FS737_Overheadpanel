c72abde808b69d94f53ac17c80358be561bcdb7f
Marcel Haupt <subcortexx@gmail.com>
2016-06-02 13:03:30 +0200

    Engine Hydraulics funktioniert nur bei fsi.SLI_GEN_1_RTL=true

--- Overheadpanel/HYDRAULICS.cs
+++ Overheadpanel/HYDRAULICS.cs
@@ -22,6 +22,9 @@ namespace Overheadpanel
             fsi.OnVarReceiveEvent += fsiOnVarReceive;
             fsi.DeclareAsWanted(new FSIID[]
                 {
+                    FSIID.SLI_GEN_1_RTL,
+                    FSIID.SLI_GEN_2_RTL,
+                    
                     FSIID.MBI_HYDRAULICS_ELEC_1_SWITCH,
                     FSIID.MBI_HYDRAULICS_ELEC_2_SWITCH,
                     FSIID.MBI_HYDRAULICS_ENG_1_SWITCH,
@@ -49,75 +52,82 @@ namespace Overheadpanel
             //ELEC 1
             if (id == FSIID.MBI_HYDRAULICS_ELEC_1_SWITCH && fsi.MBI_HYDRAULICS_ELEC_1_SWITCH == true)
             {
-                debug("HYDRAULICS ELEC 1 On");
+                if (fsi.MBI_HYDRAULICS_ELEC_1_SWITCH) {
+                    debug("HYDRAULICS ELEC 1 On");
+                } else {
+                    debug("HYDRAULICS ELEC 1 Off");
+                }
 
                 //ELT light
-                LightController.set(FSIID.MBI_HYDRAULICS_ELEC_1_LOW_PRESSURE_LIGHT, false);
-                LightController.ProcessWrites();
-            }
-            if (id == FSIID.MBI_HYDRAULICS_ELEC_1_SWITCH && fsi.MBI_HYDRAULICS_ELEC_1_SWITCH == false)
-            {
-                debug("HYDRAULICS ELEC 1 Off");
-
-                //ELT light
-                LightController.set(FSIID.MBI_HYDRAULICS_ELEC_1_LOW_PRESSURE_LIGHT, true);
+                LightController.set(FSIID.MBI_HYDRAULICS_ELEC_1_LOW_PRESSURE_LIGHT, !fsi.MBI_HYDRAULICS_ELEC_1_SWITCH);
                 LightController.ProcessWrites();
             }
 
             //ELEC 2
-            if (id == FSIID.MBI_HYDRAULICS_ELEC_2_SWITCH && fsi.MBI_HYDRAULICS_ELEC_2_SWITCH == true)
+            if (id == FSIID.MBI_HYDRAULICS_ELEC_2_SWITCH)
             {
-                debug("HYDRAULICS ELEC 2 On");
+                if (fsi.MBI_HYDRAULICS_ELEC_2_SWITCH) {
+                    debug("HYDRAULICS ELEC 2 On");
+                } else {
+                    debug("HYDRAULICS ELEC 2 Off");
+                }
 
                 //ELT light
-                LightController.set(FSIID.MBI_HYDRAULICS_ELEC_2_LOW_PRESSURE_LIGHT, false);
-                LightController.ProcessWrites();
-            }
-            if (id == FSIID.MBI_HYDRAULICS_ELEC_2_SWITCH && fsi.MBI_HYDRAULICS_ELEC_2_SWITCH == false)
-            {
-                debug("HYDRAULICS ELEC 2 Off");
-
-                //ELT light
-                LightController.set(FSIID.MBI_HYDRAULICS_ELEC_2_LOW_PRESSURE_LIGHT, true);
+                LightController.set(FSIID.MBI_HYDRAULICS_ELEC_2_LOW_PRESSURE_LIGHT, !fsi.MBI_HYDRAULICS_ELEC_2_SWITCH);
                 LightController.ProcessWrites();
             }
 
 
             //ENG 1
-            if (id == FSIID.MBI_HYDRAULICS_ENG_1_SWITCH && fsi.MBI_HYDRAULICS_ENG_1_SWITCH == true)
+            if (id == FSIID.MBI_HYDRAULICS_ENG_1_SWITCH)
             {
-                debug("HYDRAULICS ENG 1 On");
+                if (fsi.MBI_HYDRAULICS_ENG_1_SWITCH == true) {
+                    debug("HYDRAULICS ENG 1 On");
+                } else {
+                    debug("HYDRAULICS ENG 1 Off");
+                }
 
                 //ELT light
-                LightController.set(FSIID.MBI_HYDRAULICS_ENG_1_LOW_PRESSURE_LIGHT, false);
-                LightController.ProcessWrites();
-            }
-            if (id == FSIID.MBI_HYDRAULICS_ENG_1_SWITCH && fsi.MBI_HYDRAULICS_ENG_1_SWITCH == false)
-            {
-                debug("HYDRAULICS ENG 1 Off");
-
-                //ELT light
-                LightController.set(FSIID.MBI_HYDRAULICS_ENG_1_LOW_PRESSURE_LIGHT, true);
-                LightController.ProcessWrites();
+                sim_hydraulics();
             }
 
             //ENG 2
             if (id == FSIID.MBI_HYDRAULICS_ENG_2_SWITCH && fsi.MBI_HYDRAULICS_ENG_2_SWITCH == true)
             {
-                debug("HYDRAULICS ENG 2 On");
+                if (fsi.MBI_HYDRAULICS_ENG_2_SWITCH) {
+                    debug("HYDRAULICS ENG 2 On");
+                } else {
+                    debug("HYDRAULICS ENG 2 Off");
+                }
 
                 //ELT light
+                sim_hydraulics();
+            }
+            
+            
+            //engine ready to load
+            if (id == FSIID.SLI_GEN_1_RTL || id == FSIID.SLI_GEN_2_RTL) {
+                sim_hydraulics();
+            }
+        }
+        
+        
+        private static void sim_hydraulics() {
+            //gen 1 hyd pump
+            if (fsi.MBI_HYDRAULICS_ENG_1_SWITCH && fsi.SLI_GEN_1_RTL) {
+                LightController.set(FSIID.MBI_HYDRAULICS_ENG_1_LOW_PRESSURE_LIGHT, false);
+            } else {
+                LightController.set(FSIID.MBI_HYDRAULICS_ENG_1_LOW_PRESSURE_LIGHT, true);
+            }
+            
+            //gen 2 hyd pump
+            if (fsi.MBI_HYDRAULICS_ENG_2_SWITCH && fsi.SLI_GEN_2_RTL) {
                 LightController.set(FSIID.MBI_HYDRAULICS_ENG_2_LOW_PRESSURE_LIGHT, false);
-                LightController.ProcessWrites();
-            }
-            if (id == FSIID.MBI_HYDRAULICS_ENG_2_SWITCH && fsi.MBI_HYDRAULICS_ENG_2_SWITCH == false)
-            {
-                debug("HYDRAULICS ENG 2 Off");
-
-                //ELT light
+            } else {
                 LightController.set(FSIID.MBI_HYDRAULICS_ENG_2_LOW_PRESSURE_LIGHT, true);
-                LightController.ProcessWrites();
             }
+            
+            LightController.ProcessWrites();
         }
 
     }
