84bc18c9a5b35b60e5f2bf932b14e27c7275c8ea
Marcel Haupt <subcortexx@gmail.com>
2016-06-02 13:20:08 +0200

    IRS Fails when no power

--- Overheadpanel/IRS.cs
+++ Overheadpanel/IRS.cs
@@ -23,6 +23,8 @@ namespace Overheadpanel
             fsi.OnVarReceiveEvent += fsiOnVarReceive;
             fsi.DeclareAsWanted(new FSIID[]
                 {
+                    FSIID.SLI_BAT_BUS_VOLTAGE,
+                    
                     FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_OFF_POS,
                     FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_NAV_POS,
                     FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_ATT_POS,
@@ -64,80 +66,95 @@ namespace Overheadpanel
         static void fsiOnVarReceive(FSIID id)
         {
 
-            //LEFT IRS KNOB
-            if (id == FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_OFF_POS && fsi.MBI_IRS_CONTROL_L_MODE_SWITCH_OFF_POS)
+            //LEFT IRS KNOB or Power
+            if (id == FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_OFF_POS ||
+                id == FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_ALIGN_POS || 
+                id == FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_NAV_POS || 
+                id == FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_ATT_POS || 
+                id == FSIID.SLI_BAT_BUS_VOLTAGE)
             {
-                debug("IRS L OFF");
-
-                //lights
-                LightController.set(FSIID.MBI_IRS_CONTROL_L_ON_DC_LIGHT, false);
-                LightController.set(FSIID.MBI_IRS_CONTROL_L_ALIGN_LIGHT, false);
-
-                LightController.ProcessWrites();
-
-                //stop timers
-                timer_l_align.Reset();
-                timer_l_dc_on.Reset();
+                //switch to off if
+                if (fsi.MBI_IRS_CONTROL_L_MODE_SWITCH_OFF_POS || fsi.SLI_BAT_BUS_VOLTAGE <= 12)
+                {
+                    debug("IRS L OFF");
+    
+                    //lights
+                    LightController.set(FSIID.MBI_IRS_CONTROL_L_ON_DC_LIGHT, false);
+                    LightController.set(FSIID.MBI_IRS_CONTROL_L_ALIGN_LIGHT, false);
+    
+                    LightController.ProcessWrites();
+    
+                    //stop timers
+                    timer_l_align.Reset();
+                    timer_l_dc_on.Reset();
+                }
+                else if (fsi.MBI_IRS_CONTROL_L_MODE_SWITCH_ALIGN_POS)
+                {
+                    debug("IRS L ALIGN");
+    
+                    //lights
+                    LightController.set(FSIID.MBI_IRS_CONTROL_L_ALIGN_LIGHT, true);
+                    LightController.set(FSIID.MBI_IRS_CONTROL_L_ON_DC_LIGHT, true);
+                    LightController.ProcessWrites();
+    
+                    //start aligning timers
+                    timer_l_align.Start();
+                    timer_l_dc_on.Start();
+                }
+                else if (fsi.MBI_IRS_CONTROL_L_MODE_SWITCH_NAV_POS)
+                {
+                    debug("IRS L NAV");
+                }
+                else if (fsi.MBI_IRS_CONTROL_L_MODE_SWITCH_ATT_POS)
+                {
+                    debug("IRS L ATT");
+                }
             }
-            if (id == FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_ALIGN_POS && fsi.MBI_IRS_CONTROL_L_MODE_SWITCH_ALIGN_POS)
-            {
-                debug("IRS L ALIGN");
-
-                //lights
-                LightController.set(FSIID.MBI_IRS_CONTROL_L_ALIGN_LIGHT, true);
-                LightController.set(FSIID.MBI_IRS_CONTROL_L_ON_DC_LIGHT, true);
-                LightController.ProcessWrites();
-
-                //start aligning timers
-                timer_l_align.Start();
-                timer_l_dc_on.Start();
-            }
-            if (id == FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_NAV_POS && fsi.MBI_IRS_CONTROL_L_MODE_SWITCH_NAV_POS)
-            {
-                debug("IRS L NAV");
-            }
-            if (id == FSIID.MBI_IRS_CONTROL_L_MODE_SWITCH_ATT_POS && fsi.MBI_IRS_CONTROL_L_MODE_SWITCH_ATT_POS)
-            {
-                debug("IRS L ATT");
-            }
-
 
 
             //RIGHT IRS KNOB
-            if (id == FSIID.MBI_IRS_CONTROL_R_MODE_SWITCH_OFF_POS && fsi.MBI_IRS_CONTROL_R_MODE_SWITCH_OFF_POS)
+            if (id == FSIID.MBI_IRS_CONTROL_R_MODE_SWITCH_OFF_POS ||
+                id == FSIID.MBI_IRS_CONTROL_R_MODE_SWITCH_ALIGN_POS || 
+                id == FSIID.MBI_IRS_CONTROL_R_MODE_SWITCH_NAV_POS || 
+                id == FSIID.MBI_IRS_CONTROL_R_MODE_SWITCH_ATT_POS || 
+                id == FSIID.SLI_BAT_BUS_VOLTAGE)
             {
-                debug("IRS R OFF");
-
-                //lights
-                LightController.set(FSIID.MBI_IRS_CONTROL_R_ON_DC_LIGHT, false);
-                LightController.set(FSIID.MBI_IRS_CONTROL_R_ALIGN_LIGHT, false);
-
-                LightController.ProcessWrites();
-
-                //stop timers
-                timer_r_align.Reset();
-                timer_r_dc_on.Reset();
-            }
-            if (id == FSIID.MBI_IRS_CONTROL_R_MODE_SWITCH_ALIGN_POS && fsi.MBI_IRS_CONTROL_R_MODE_SWITCH_ALIGN_POS)
-            {
-                debug("IRS R ALIGN");
-
-                //lights
-                LightController.set(FSIID.MBI_IRS_CONTROL_R_ALIGN_LIGHT, true);
-                LightController.set(FSIID.MBI_IRS_CONTROL_R_ON_DC_LIGHT, true);
-                LightController.ProcessWrites();
-
-                //start aligning timers
-                timer_r_align.Start();
-                timer_r_dc_on.Start();
-            }
-            if (id == FSIID.MBI_IRS_CONTROL_R_MODE_SWITCH_NAV_POS && fsi.MBI_IRS_CONTROL_R_MODE_SWITCH_NAV_POS)
-            {
-                debug("IRS R NAV");
-            }
-            if (id == FSIID.MBI_IRS_CONTROL_R_MODE_SWITCH_ATT_POS && fsi.MBI_IRS_CONTROL_R_MODE_SWITCH_ATT_POS)
-            {
-                debug("IRS R ATT");
+                
+                if (fsi.MBI_IRS_CONTROL_R_MODE_SWITCH_OFF_POS || fsi.SLI_BAT_BUS_VOLTAGE <= 12)
+                {
+                    debug("IRS R OFF");
+    
+                    //lights
+                    LightController.set(FSIID.MBI_IRS_CONTROL_R_ON_DC_LIGHT, false);
+                    LightController.set(FSIID.MBI_IRS_CONTROL_R_ALIGN_LIGHT, false);
+    
+                    LightController.ProcessWrites();
+    
+                    //stop timers
+                    timer_r_align.Reset();
+                    timer_r_dc_on.Reset();
+                }
+                else if (fsi.MBI_IRS_CONTROL_R_MODE_SWITCH_ALIGN_POS)
+                {
+                    debug("IRS R ALIGN");
+    
+                    //lights
+                    LightController.set(FSIID.MBI_IRS_CONTROL_R_ALIGN_LIGHT, true);
+                    LightController.set(FSIID.MBI_IRS_CONTROL_R_ON_DC_LIGHT, true);
+                    LightController.ProcessWrites();
+    
+                    //start aligning timers
+                    timer_r_align.Start();
+                    timer_r_dc_on.Start();
+                }
+                else if (fsi.MBI_IRS_CONTROL_R_MODE_SWITCH_NAV_POS)
+                {
+                    debug("IRS R NAV");
+                }
+                else if (fsi.MBI_IRS_CONTROL_R_MODE_SWITCH_ATT_POS)
+                {
+                    debug("IRS R ATT");
+                }
             }
 
            
