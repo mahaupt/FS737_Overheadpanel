984008b18872f43beba03685b95565baae3c20ae
Marcel Haupt <subcortexx@gmail.com>
2016-06-07 09:46:25 +0200

    FSIcm added

---
+++ FSToolbox/FSIcm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FSInterface;
+
+
+//static fsi client for all panels to use
+
+namespace FSToolbox
+{
+    public class FSIcm : FSIClient
+    {
+        public static FSIcm inst = null;
+
+        public FSIcm(String desc) : base(desc)
+        {
+            inst = this;
+        }
+    }
+}
